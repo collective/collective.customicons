1e31d64988f148babd1dadea8d2a08e97e4e3c7e
benniboy <bs@kleinundpartner.at>
2013-09-02 13:03:09 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1'
+version = '1.2'
 shortdesc = 'make custom icons available in plone'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
